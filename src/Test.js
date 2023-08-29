1e3fab75adbdab1601300c81c81631b0651e9d31
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-08-29 07:44:16 +0530

    Added Code Optimization in Test.js using children trick

--- src/Test.js
+++ src/Test.js
@@ -14,13 +14,33 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+  return (
+    <div>
+      <h1>Slow Counter ?!?</h1>
+      <Counter>
+        {/* Passed SlowComponent as a children prop now it will be created before Counter so it will not get re-render on every Counter State Update which optimizes the code alot*/}
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
